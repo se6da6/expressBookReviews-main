1e97bed6cd3c665aab1467637f0c1a318541e67b
se6da6 <sedadak06@gmail.com>
2023-08-04 15:12:53 -0400

    publish all changes

--- final_project/index.js
+++ final_project/index.js
@@ -4,15 +4,50 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+let users = []
+//Function to check if the user exists
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+//Function to check if the user is authenticated
+const authenticatedUser = (username,password)=>{
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
 const app = express();
 
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session.authorization && req.session.authorization.accessToken) {
+      const token = req.session.authorization.accessToken;
+      jwt.verify(token, "access", (err, decoded) => {
+        if (!err) {
+          req.user = decoded.data; // Set the user data from the decoded token
+          next();
+        } else {
+          return res.status(403).json({ message: "User not authenticated" });
+        }
+      });
+    } else {
+      return res.status(403).json({ message: "User not logged in" });
+    }
+  });
  
 const PORT =5000;
 
